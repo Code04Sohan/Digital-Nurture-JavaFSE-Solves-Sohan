29a13daef8170bdfad573d4c45cc163a1321242a
SohanCode007 <96377062+Code04Sohan@users.noreply.github.com>
2026-07-05 13:50:20 +0530

    React HOL

---
+++ [Solves] DeepSkill/[Solves] React/studentapp/src/About.js
@@ -0,0 +1,9 @@
+import React, {Component} from React;
+
+import class Home extends Component {
+    render(){
+        <div>
+        <h3>Welcome to About Page</h3>
+        </div>
+    }
+}
---
+++ [Solves] DeepSkill/[Solves] React/studentapp/src/Contact.js
@@ -0,0 +1,9 @@
+import React, {Component} from React;
+
+import class Home extends Component {
+    render(){
+        <div>
+        <h3>Welcome to Contact Page</h3>
+        </div>
+    }
+}
